97018994d5e6ec174c9ad7332b93387586fa065e
ANAND <anand.121991@gmail.com>
2017-06-11 14:51:53 +0530

    grdi data

--- static/js/stock.js
+++ static/js/stock.js
@@ -1,13 +1,40 @@
 var stock = angular.module("stock",[]);
 var stockList=["PIH","FLWS","FCCY","XLRN","AKAO"];
 
-stock.controller('stockController', ['$scope', function($scope){
+stock.controller('stockController', ['$scope','$http', function($scope,$http){
 
 	$scope.companyList = stockList;
+	$scope.searchList= [];
 
 	$scope.getCompayStock= function(){
-		alert(this);
+		var baseUrl='http://finance.google.com/finance/info?client=ig&q=NASDAQ%3A';
+		var com = this.company;
+		
+		$scope.companyList = removeElem($scope.companyList,com);
+		$scope.searchList.push(com);
+		
+
+			$http({
+				method: 'GET',
+				url: baseUrl+$scope.searchList
+
+			}).then(function onSuccess(response){
+				var stockArry=response.data.replace("//","");			
+				$scope.stockData=JSON.parse(stockArry);
+				
+
+			},function onError(response){
+				alert("Error....!!!");
+			});
 	}
+
+	function removeElem(elemArr,elem){
+	
+		elemArr.splice(elemArr.indexOf(elem),1);
+		return elemArr;
+
+	}
+	f
 	
 }]);
 
